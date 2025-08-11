a3e3ffc0e2d9b2efe05392fe13522afc7101af24
Catalyna Dunsmore <cata.dunsmore@gmail.com>
2025-08-11 17:12:40 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,9 +8,9 @@ function setup() {
 function draw() {
   d= random(10,60)
   
-  colorMode(HSB,360,100,100);
-  fill(random(0,360),100,100);
-  ellipse(mouseX,mouseY,d,d);
+colorMode(HSB,360,100,100);
+fill(random(0,360),100,100);
+ellipse(mouseX,mouseY,d,d);
 }
 function keyPressed() {
   if (key === 'p') {
